d9af15b6e94b851d6495ba119a909d5ec2528701
Angharad Hill <h017026h@student.staffs.ac.uk>
2022-01-28 19:05:08 +0000

    First Person Player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public Camera mPlayerCamera;
+    public CharacterController mPlayerController;
+    Vector3 mPlayerVelocity;
+    bool mbPlayerGrounded;
+
+    public float speed = 12f;
+    public float gravity = 9.81f;
+    public float jumpHeight = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mbPlayerGrounded = mPlayerController.isGrounded;
+        
+        if (mbPlayerGrounded && mPlayerVelocity.y <0)
+        {
+            mPlayerVelocity.y = -2;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        mPlayerController.Move(move * speed * Time.deltaTime);
+
+
+        if(Input.GetButtonDown("Jump") && mbPlayerGrounded)
+        {
+            mPlayerVelocity.y = Mathf.Sqrt(jumpHeight * 2 * gravity);
+        }
+
+
+        mPlayerVelocity.y -= gravity * Time.deltaTime;
+
+        mPlayerController.Move(mPlayerVelocity * Time.deltaTime);
+
+       
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
