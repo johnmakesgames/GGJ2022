d3d380721ccae4f4c05d5349eb75dca469b38c5d
Taylor Fahy <taylorfahy1337@gmail.com>
2022-01-29 14:13:28 +0000

    Added teleporter UI

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Teleporter : MonoBehaviour
 {
 
     public Transform teleportTargert;
     public GameObject playerObject;
+    [SerializeField] private Image teleporterUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        teleporterUI.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        teleporterUI.enabled = true;
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        teleporterUI.enabled = false;
+    }
+
+
 
     public void ActivateTeleport()
     {
