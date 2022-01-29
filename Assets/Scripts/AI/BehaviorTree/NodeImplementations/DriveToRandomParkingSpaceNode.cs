67b89ef51ce706c0cb02649e74bbbae36b70038d
John Whiles <john.whiles@hotmail.com>
2022-01-29 20:05:28 +0000

    Created pedestrian prefabs included assassination target

--- Assets/Scripts/AI/BehaviorTree/NodeImplementations/DriveToRandomParkingSpaceNode.cs
+++ Assets/Scripts/AI/BehaviorTree/NodeImplementations/DriveToRandomParkingSpaceNode.cs
@@ -22,7 +22,6 @@ public class DriveToRandomParkingSpaceNode : ActionNode
     public override NodeStates OnUpdate()
     {
         float distance = Vector3.Distance(parent.transform.position, positionToGoTo);
-        Debug.Log($"Distance to Goal {distance}");
 
         if (parent.GetComponent<UnityEngine.AI.NavMeshAgent>().isOnOffMeshLink && !onLink)
         {
