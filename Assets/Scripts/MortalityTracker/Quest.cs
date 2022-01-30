d6f6bf031284d6cae4d1bddd612d5c58d36d2a7c
John Whiles <john.whiles@hotmail.com>
2022-01-30 15:11:31 +0000

    UI Sizing changed, man with fish now

--- Assets/Scripts/MortalityTracker/Quest.cs
+++ Assets/Scripts/MortalityTracker/Quest.cs
@@ -27,6 +27,7 @@ public class Quest
                 GameObject.Find("PlayerManager")?.GetComponent<PlayerManager>()?.SetAsActivePlayer("FirstPersonPlayer");
                 break;
             case GoalType.Fishing:
+                GameObject.Find("Pedestrian_HasFish")?.SetActive(true);
                 GameObject.Find("Pedestrian_Sitting 1")?.SetActive(false);
                 break;
             case GoalType.Unpacking:
